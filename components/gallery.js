a26c5514eacb4b38790fdb51e7841098e8328f3a
Max Wofford <max@maxwofford.com>
2021-05-28 23:33:33 -0400

    Move gallery into component

---
+++ components/gallery.js
@@ -0,0 +1,69 @@
+import { Grid, Image } from 'theme-ui'
+const Gallery = () => (
+  <Container>
+    <Grid columns={['2fr 3fr']} sx={{ my: '1em' }}>
+      <Image
+        src="https://cloud-3q64c1iul-hack-club-bot.vercel.app/0mark_twain_zephyr_ya.png"
+        sx={{ objectFit: 'cover', borderRadius: '4px', height: '250px' }}
+      />
+      <Grid columns={3}>
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/2/24/Essayons_in_New_York_Harbor.jpg"
+          sx={{
+            objectFit: 'cover',
+            borderRadius: '4px',
+            height: 'calc(234px / 2)',
+            width: '100%'
+          }}
+        />
+        <Image
+          src="https://261.com/wp-content/gallery/super-dome/imgp0900web.jpg"
+          sx={{
+            objectFit: 'cover',
+            borderRadius: '4px',
+            height: 'calc(234px / 2)',
+            width: '100%'
+          }}
+        />
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Vermont_Academy%2C_Saxtons_River%2C_Vermont.jpg/640px-Vermont_Academy%2C_Saxtons_River%2C_Vermont.jpg"
+          sx={{
+            objectFit: 'cover',
+            borderRadius: '4px',
+            height: 'calc(234px / 2)',
+            width: '100%'
+          }}
+        />
+        <Image
+          src="https://i.pinimg.com/originals/eb/d8/ed/ebd8edaa5fcaf01254ef442c8f3627b1.jpg"
+          sx={{
+            objectFit: 'cover',
+            borderRadius: '4px',
+            height: 'calc(234px / 2)',
+            width: '100%'
+          }}
+        />
+        <Image
+          src="https://api.time.com/wp-content/uploads/2015/01/483781925.jpg"
+          sx={{
+            objectFit: 'cover',
+            borderRadius: '4px',
+            height: 'calc(234px / 2)',
+            width: '100%'
+          }}
+        />
+        <Image
+          src="http://i.imgur.com/gxP87hS.jpg"
+          sx={{
+            objectFit: 'cover',
+            borderRadius: '4px',
+            height: 'calc(234px / 2)',
+            width: '100%'
+          }}
+        />
+      </Grid>
+    </Grid>
+  </Container>
+)
+
+export default Gallery
--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ import Meta from '../components/meta'
 import Header from '../components/head'
 import CTA from '../components/cta'
 import Telegram from '../components/telegram'
+import Gallery from '../components/gallery'
 // import MultiplayerMouse from '../components/multiplayer-mouse'
 
 export default function App() {
@@ -64,69 +65,8 @@ export default function App() {
       <Header />
       <Telegram />
       <CTA />
+      <Gallery />
       <Container>
-        <Grid columns={['2fr 3fr']} sx={{ my: '1em' }}>
-          <Image
-            src="https://cloud-3q64c1iul-hack-club-bot.vercel.app/0mark_twain_zephyr_ya.png"
-            sx={{ objectFit: 'cover', borderRadius: '4px', height: '250px' }}
-          />
-          <Grid columns={3}>
-            <Image
-              src="https://upload.wikimedia.org/wikipedia/commons/2/24/Essayons_in_New_York_Harbor.jpg"
-              sx={{
-                objectFit: 'cover',
-                borderRadius: '4px',
-                height: 'calc(234px / 2)',
-                width: '100%'
-              }}
-            />
-            <Image
-              src="https://261.com/wp-content/gallery/super-dome/imgp0900web.jpg"
-              sx={{
-                objectFit: 'cover',
-                borderRadius: '4px',
-                height: 'calc(234px / 2)',
-                width: '100%'
-              }}
-            />
-            <Image
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Vermont_Academy%2C_Saxtons_River%2C_Vermont.jpg/640px-Vermont_Academy%2C_Saxtons_River%2C_Vermont.jpg"
-              sx={{
-                objectFit: 'cover',
-                borderRadius: '4px',
-                height: 'calc(234px / 2)',
-                width: '100%'
-              }}
-            />
-            <Image
-              src="https://i.pinimg.com/originals/eb/d8/ed/ebd8edaa5fcaf01254ef442c8f3627b1.jpg"
-              sx={{
-                objectFit: 'cover',
-                borderRadius: '4px',
-                height: 'calc(234px / 2)',
-                width: '100%'
-              }}
-            />
-            <Image
-              src="https://api.time.com/wp-content/uploads/2015/01/483781925.jpg"
-              sx={{
-                objectFit: 'cover',
-                borderRadius: '4px',
-                height: 'calc(234px / 2)',
-                width: '100%'
-              }}
-            />
-            <Image
-              src="http://i.imgur.com/gxP87hS.jpg"
-              sx={{
-                objectFit: 'cover',
-                borderRadius: '4px',
-                height: 'calc(234px / 2)',
-                width: '100%'
-              }}
-            />
-          </Grid>
-        </Grid>
         <Heading as="h1" my={[1, 3]}>Appendix</Heading>
         <Grid columns={2}>
           {questions.map(({ question, answer }, index) => (
