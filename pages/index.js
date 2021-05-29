1253dcac232dd094da2d52df610598d2c8955fd2
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-05-29 12:11:26 +0800

    Questions to Lib

--- pages/index.js
+++ pages/index.js
@@ -14,48 +14,9 @@ import CTA from '../components/cta'
 import Telegram from '../components/telegram'
 import Gallery from '../components/gallery'
 // import MultiplayerMouse from '../components/multiplayer-mouse'
+import questions from '../lib/questions'
 
 export default function App() {
-  let questions = [
-    {
-      question: 'Who is eligible to board the train?',
-      answer:
-        'All members of the Hack Club Slack who are fully vaccinated by the boarding date are eligible to board the train. Limited tickets are available, though.'
-    },
-    {
-      question: 'My parents have concerns, how can I convince them?',
-      answer: (
-        <>
-          We have prepared a guide for parents. You can access it here. If
-          needed, you're parents can also contact us at team@hackclub.com
-        </>
-      )
-    },
-    {
-      question: 'Will housing, food and drink be provided?',
-      answer:
-        'Yes. All essentials will be covered by us. Housing will take place in both sleeper trains and reputable hotels.'
-    },
-    {
-      question: 'What will I need to participate?',
-      answer:
-        'We recommend bringing clothing, toiletries and electronics. A more extensive list will be provided at a later date.'
-    },
-    {
-      question: 'Will travel to Burlington and from LA be covered?',
-      answer:
-        'Yes, we will reemburse the costs of flights and / or trains required to get to Burlington and home from LA. '
-    },
-    {
-      question: 'How can I get in touch?',
-      answer: (
-        <>
-          If you've got more questions, feel free to contact the team at
-          team@hackclub.com and we will do our best to help.
-        </>
-      )
-    }
-  ]
   return (
     <Box
       sx={{ bg: '#C44D4D', minHeight: '100vh', color: 'white', py: 4, pt: 0, overflowX: 'hidden' }}
@@ -158,6 +119,9 @@ export default function App() {
           max-width: 100vw;
           overflow-x: hidden;
           width: 100vw;
+        }
+        a{
+          color: inherit
         }
         `}
       </style>
