163ee4c245c4bbd90a5b1a473e4975ffaa4f50e4
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-05-29 12:16:59 +0800

    Compartmentilse Footer

---
+++ components/footer.js
@@ -0,0 +1,69 @@
+import {
+  Box,
+  Container,
+  Heading,
+  Text,
+  Grid,
+  Flex,
+  Link
+} from 'theme-ui'
+
+export default function Footer() {
+  return (
+    <Container>
+      <Grid sx={{ pt: 5 }} columns={'2.8fr 1fr'}>
+        <Flex sx={{ alignItems: 'center' }}>
+          <Box>
+            <Text as="p" sx={{ fontWeight: '800', pb: 1 }}>
+              A Project by Hack Club
+            </Text>
+            <Text as="p">
+              <Link sx={{ color: 'white' }} href="https://hackclub.com">
+                Home
+              </Link>{' '}
+              |{' '}
+              <Link sx={{ color: 'white' }} href="https://hackclub.com/slack">
+                Slack
+              </Link>{' '}
+              |{' '}
+              <Link
+                sx={{ color: 'white' }}
+                href="https://workshops.hackclub.com"
+              >
+                Workshops
+              </Link>{' '}
+              |{' '}
+              <Link
+                sx={{ color: 'white' }}
+                href="https://scrapbook.hackclub.com"
+              >
+                Scrapbook
+              </Link>
+            </Text>
+          </Box>
+        </Flex>
+        <Box sx={{ textAlign: 'right' }}>
+          <Heading
+            sx={{
+              fontSize: '3em',
+              float: 'right'
+            }}
+          >
+            <Box
+              sx={{
+                border: '5px solid white',
+                width: 'min-content',
+                px: 2,
+                borderRadius: 5,
+                width: '64px',
+                textAlign: 'center'
+              }}
+            >
+              h
+            </Box>
+          </Heading>
+        </Box>
+      </Grid>
+    </Container>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -13,13 +13,21 @@ import Header from '../components/head'
 import CTA from '../components/cta'
 import Telegram from '../components/telegram'
 import Gallery from '../components/gallery'
+import Footer from '../components/footer'
 // import MultiplayerMouse from '../components/multiplayer-mouse'
 import questions from '../lib/questions'
 
 export default function App() {
   return (
     <Box
-      sx={{ bg: '#C44D4D', minHeight: '100vh', color: 'white', py: 4, pt: 0, overflowX: 'hidden' }}
+      sx={{
+        bg: '#C44D4D',
+        minHeight: '100vh',
+        color: 'white',
+        py: 4,
+        pt: 0,
+        overflowX: 'hidden'
+      }}
     >
       <Meta />
       {/* <MultiplayerMouse /> */}
@@ -27,8 +35,10 @@ export default function App() {
       <Telegram />
       <CTA />
       <Gallery />
-      <Container>
-        <Heading as="h1" my={[1, 3]}>Appendix</Heading>
+      <Container sx={{ mt: 2}}>
+        <Heading as="h1" my={[1, 3]}>
+          Appendix
+        </Heading>
         <Grid columns={2}>
           {questions.map(({ question, answer }, index) => (
             <Box
@@ -44,60 +54,8 @@ export default function App() {
             </Box>
           ))}
         </Grid>
-        <Grid sx={{ pt: 5 }} columns={'2.8fr 1fr'}>
-          <Flex sx={{ alignItems: 'center' }}>
-            <Box>
-              <Text as="p" sx={{ fontWeight: '800', pb: 1 }}>
-                A Project by Hack Club
-              </Text>
-              <Text as="p">
-                <Link sx={{ color: 'white' }} href="https://hackclub.com">
-                  Home
-                </Link>{' '}
-                |{' '}
-                <Link sx={{ color: 'white' }} href="https://hackclub.com/slack">
-                  Slack
-                </Link>{' '}
-                |{' '}
-                <Link
-                  sx={{ color: 'white' }}
-                  href="https://workshops.hackclub.com"
-                >
-                  Workshops
-                </Link>{' '}
-                |{' '}
-                <Link
-                  sx={{ color: 'white' }}
-                  href="https://scrapbook.hackclub.com"
-                >
-                  Scrapbook
-                </Link>
-              </Text>
-            </Box>
-          </Flex>
-          <Box sx={{ textAlign: 'right' }}>
-            <Heading
-              sx={{
-                fontSize: '3em',
-                float: 'right'
-              }}
-            >
-              <Box
-                sx={{
-                  border: '5px solid white',
-                  width: 'min-content',
-                  px: 2,
-                  borderRadius: 5,
-                  width: '64px',
-                  textAlign: 'center'
-                }}
-              >
-                h
-              </Box>
-            </Heading>
-          </Box>
-        </Grid>
       </Container>
+      <Footer />
       <style>
         {`
         .cursor {
