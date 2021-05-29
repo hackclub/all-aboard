6f43c7d00719bd07ebfeeb40ea4d062138022272
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-05-29 11:35:07 +0800

    add links

--- components/cta.js
+++ components/cta.js
@@ -1,4 +1,4 @@
-import { Box, Heading, Text, Button } from "theme-ui"
+import { Box, Heading, Text, Button } from 'theme-ui'
 
 const buttonSX = {
   background: '#e28a89',
@@ -6,24 +6,31 @@ const buttonSX = {
   minWidth: [5, 8],
   border: 'none',
   m: 3,
-  p: 3,
+  mt: 4, 
+  p: 4
 }
 
 const CTA = () => (
-  <Box sx={{
-    textAlign: 'center',
-    m: 5
-  }}>
-    <Heading sx={{fontSize: 7}}>Apply Now</Heading>
+  <Box
+    sx={{
+      textAlign: 'center',
+      m: 5
+    }}
+  >
+    <Heading sx={{ fontSize: 7 }}>Apply Now</Heading>
     <Button sx={buttonSX} as="a" href="/api/sign-in-slack" target="_blank">
-      <Heading sx={{mb: [1, 3]}}>Hacker Zephyr</Heading>
-      <Box sx={{display: 'flex'}}>
-        <Text sx={{textTransform: 'uppercase'}}><Text sx={{fontWeight: 'bold'}}>0¢</Text> Fare</Text>
-        <Text>|</Text>
+      <Heading sx={{ mb: [1, 3] }}>Hacker Zephyr</Heading>
+      <Box sx={{ display: 'flex' }}>
+        <Text sx={{ textTransform: 'uppercase' }}>
+          <Text sx={{ fontWeight: 'bold' }}>0¢</Text> Fare
+        </Text>
+        <Text sx={{ mx: '4px'}}>|</Text>
         <Text>One-Way Hacker</Text>
       </Box>
     </Button>
-    <Text as="p" sx={{fontStyle: 'italic'}}>Open until July 12th, 2021</Text>
+    <Text as="p" sx={{ fontStyle: 'italic' }}>
+      Open until July 12th, 2021
+    </Text>
   </Box>
 )
 
--- components/head.js
+++ components/head.js
@@ -5,14 +5,24 @@ import useHasMounted from '../lib/use-has-mounted'
 const Static = () => (
   <Box
     as="section"
-    sx={{ overflow: 'hidden', position: 'relative', bg: '#011C36', height: ['60vh', '100vh'], textAlign: 'center' }}
+    sx={{
+      overflow: 'hidden',
+      position: 'relative',
+      bg: '#011C36',
+      height: ['60vh', '100vh'],
+      textAlign: 'center'
+    }}
   >
-    <Image src="/header.png" alt="This summer, we're closing Zoom. All aboard the Hacker Zephyr. Trans America, Burlington Vermont - Los Angeles California" sx={{
-            mt: '5vh',
-            height: ['50vh !important', '90vh !important'],
-            maxHeight: '90vh !important',
-            bg: '#011C36'
-          }} />
+    <Image
+      src="/header.png"
+      alt="This summer, we're closing Zoom. All aboard the Hacker Zephyr. Trans America, Burlington Vermont - Los Angeles California"
+      sx={{
+        mt: '5vh',
+        height: ['50vh !important', '90vh !important'],
+        maxHeight: '90vh !important',
+        bg: '#011C36'
+      }}
+    />
   </Box>
 )
 
@@ -23,7 +33,13 @@ const MAINPART = () => {
     return (
       <Box
         as="section"
-        sx={{ overflow: 'hidden', position: 'relative', bg: '#011C36', height: ['60vh', '100vh'], width: '100vw' }}
+        sx={{
+          overflow: 'hidden',
+          position: 'relative',
+          bg: '#011C36',
+          height: ['60vh', '100vh'],
+          width: '100vw'
+        }}
       >
         <Box
           as="video"
@@ -42,10 +58,12 @@ const MAINPART = () => {
             width: '100% !important',
             maxHeight: '90vh !important'
           }}
-          
         >
           <source src="/Untitled Project 300000.mp4" />
-          <p>This summer, we're closing Zoom. All aboard the Hacker Zephyr. Trans America, Burlington Vermont - Los Angeles California</p>
+          <p>
+            This summer, we're closing Zoom. All aboard the Hacker Zephyr. Trans
+            America, Burlington Vermont - Los Angeles California
+          </p>
         </Box>
       </Box>
     )
