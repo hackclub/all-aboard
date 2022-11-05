9e7b5ded24c46de1ef6fc2805ac331e9cb00727e
Sam Poder <sam.r.poder@gmail.com>
2022-11-05 11:54:19 +0800

    Add Plausible

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,14 @@ import NextApp from 'next/app'
 import theme from '../lib/theme'
 import { ThemeProvider } from 'theme-ui'
 import '../styles/globals.css'
+import Script from 'next/script'
 
 export default class App extends NextApp {
   render() {
     const { Component, pageProps } = this.props
     return (
       <ThemeProvider theme={theme}>
+        <Script defer data-domain="zephyr.hackclub.com" src="https://plausible.io/js/script.js" />
         <Component {...pageProps} />
       </ThemeProvider>
     )
